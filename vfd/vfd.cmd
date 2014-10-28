8449ac9ce574427af7e8235fcb7fab6d11f241ba
iev <iev@land.ru>
2014-10-28 18:34:23 +0300

    vfd pcb v1

--- vfd/vfd.cmd
+++ vfd/vfd.cmd
@@ -1,22 +1,14 @@
 # Pin name action command file
 
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
 # Start of element S5
 ChangePinName(S5, 4, 4)
 ChangePinName(S5, 2, 2)
 ChangePinName(S5, 3, 3)
 ChangePinName(S5, 1, 1)
-
-# Start of element S1
-ChangePinName(S1, 4, 4)
-ChangePinName(S1, 2, 2)
-ChangePinName(S1, 3, 3)
-ChangePinName(S1, 1, 1)
-
-# Start of element S2
-ChangePinName(S2, 4, 4)
-ChangePinName(S2, 2, 2)
-ChangePinName(S2, 3, 3)
-ChangePinName(S2, 1, 1)
 
 # Start of element S4
 ChangePinName(S4, 4, 4)
@@ -24,30 +16,90 @@ ChangePinName(S4, 2, 2)
 ChangePinName(S4, 3, 3)
 ChangePinName(S4, 1, 1)
 
-# Start of element J3
-ChangePinName(J3, 1, 1)
-ChangePinName(J3, 2, 2)
+# Start of element S3
+ChangePinName(S3, 4, 4)
+ChangePinName(S3, 2, 2)
+ChangePinName(S3, 3, 3)
+ChangePinName(S3, 1, 1)
+
+# Start of element S2
+ChangePinName(S2, 4, 4)
+ChangePinName(S2, 2, 2)
+ChangePinName(S2, 3, 3)
+ChangePinName(S2, 1, 1)
+
+# Start of element S1
+ChangePinName(S1, 4, 4)
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 1, GND)
+ChangePinName(U2, 4, OUT)
+ChangePinName(U2, 2, OUT)
+ChangePinName(U2, 3, IN)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element FB1
+ChangePinName(FB1, 2, 2)
+ChangePinName(FB1, 1, 1)
+
+# Start of element U3
+ChangePinName(U3, 10, 10)
+ChangePinName(U3, 9, 9)
+ChangePinName(U3, 8, 8)
+ChangePinName(U3, 7, 7)
+ChangePinName(U3, 6, 6)
+ChangePinName(U3, 5, 5)
+ChangePinName(U3, 4, 4)
+ChangePinName(U3, 3, 3)
+ChangePinName(U3, 2, GND)
+ChangePinName(U3, 1, GND)
+
+# Start of element VFD_CN2_ASYNC
+ChangePinName(VFD_CN2_ASYNC, 1, 1)
+ChangePinName(VFD_CN2_ASYNC, 2, 2)
+ChangePinName(VFD_CN2_ASYNC, 3, 3)
+ChangePinName(VFD_CN2_ASYNC, 6, 6)
+ChangePinName(VFD_CN2_ASYNC, 5, 5)
+ChangePinName(VFD_CN2_ASYNC, 4, 4)
+
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 8, OE)
+ChangePinName(U1, 9, NC)
+ChangePinName(U1, 10, B4)
+ChangePinName(U1, 11, B3)
+ChangePinName(U1, 12, B2)
+ChangePinName(U1, 13, B1)
+ChangePinName(U1, 14, Vccb)
+ChangePinName(U1, 7, GND)
+ChangePinName(U1, 6, NC)
+ChangePinName(U1, 5, A4)
+ChangePinName(U1, 4, A3)
+ChangePinName(U1, 3, A2)
+ChangePinName(U1, 2, A1)
+ChangePinName(U1, 1, Vcca)
 
 # Start of element J2
 ChangePinName(J2, 1, 1)
 ChangePinName(J2, 3, 3)
 ChangePinName(J2, 2, 2)
-
-# Start of element U2
-ChangePinName(U2, 8, OE)
-ChangePinName(U2, 9, NC)
-ChangePinName(U2, 10, B4)
-ChangePinName(U2, 11, B3)
-ChangePinName(U2, 12, B2)
-ChangePinName(U2, 13, B1)
-ChangePinName(U2, 14, Vccb)
-ChangePinName(U2, 7, GND)
-ChangePinName(U2, 6, NC)
-ChangePinName(U2, 5, A4)
-ChangePinName(U2, 4, A3)
-ChangePinName(U2, 3, A2)
-ChangePinName(U2, 2, A1)
-ChangePinName(U2, 1, Vcca)
 
 # Start of element R15
 ChangePinName(R15, 1, 1)
@@ -78,36 +130,6 @@ ChangePinName(J1, 1, 1)
 ChangePinName(J1, 4, 4)
 ChangePinName(J1, 3, 3)
 ChangePinName(J1, 2, 2)
-
-# Start of element S3
-ChangePinName(S3, 4, 4)
-ChangePinName(S3, 2, 2)
-ChangePinName(S3, 3, 3)
-ChangePinName(S3, 1, 1)
-
-# Start of element R12
-ChangePinName(R12, 1, 1)
-ChangePinName(R12, 2, 2)
-
-# Start of element R11
-ChangePinName(R11, 1, 1)
-ChangePinName(R11, 2, 2)
-
-# Start of element R10
-ChangePinName(R10, 1, 1)
-ChangePinName(R10, 2, 2)
-
-# Start of element R9
-ChangePinName(R9, 1, 1)
-ChangePinName(R9, 2, 2)
-
-# Start of element R8
-ChangePinName(R8, 1, 1)
-ChangePinName(R8, 2, 2)
-
-# Start of element R7
-ChangePinName(R7, 1, 1)
-ChangePinName(R7, 2, 2)
 
 # Start of element C6
 ChangePinName(C6, 2, 2)
@@ -156,11 +178,3 @@ ChangePinName(C1, 1, 1)
 # Start of element R1
 ChangePinName(R1, 1, 1)
 ChangePinName(R1, 2, 2)
-
-# Start of element VFD
-ChangePinName(VFD, 1, 1)
-ChangePinName(VFD, 2, 2)
-ChangePinName(VFD, 3, 3)
-ChangePinName(VFD, 6, 6)
-ChangePinName(VFD, 5, 5)
-ChangePinName(VFD, 4, 4)
