08441803b5f37290b788bc082e4d565097c69422
iev <iev@land.ru>
2014-11-15 16:50:35 +0300

    some stm32v2 upd...

---
+++ eeprom/eeprom.cmd
@@ -0,0 +1,51 @@
+# Pin name action command file
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 8, Vcc)
+ChangePinName(U1, 7, WP)
+ChangePinName(U1, 6, SCL)
+ChangePinName(U1, 5, SDA)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, A2)
+ChangePinName(U1, 2, A1)
+ChangePinName(U1, 1, A0)
+
+# Start of element J1
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 10, 10)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
