053ddfbb7129d86080706ff4aee589c7814e652b
iev <iev@land.ru>
2015-02-02 20:42:56 +0300

    added airwick.prj

---
+++ airwick/airwick.cmd
@@ -0,0 +1,137 @@
+# Pin name action command file
+
+# Start of element J6
+ChangePinName(J6, 1, 1)
+ChangePinName(J6, 4, 4)
+ChangePinName(J6, 3, 3)
+ChangePinName(J6, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element J5
+ChangePinName(J5, 1, 1)
+ChangePinName(J5, 2, 2)
+
+# Start of element J4
+ChangePinName(J4, 1, 1)
+ChangePinName(J4, 2, 2)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element J3
+ChangePinName(J3, 3, 3)
+ChangePinName(J3, 2, 2)
+ChangePinName(J3, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 3, 3)
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 1, GND)
+ChangePinName(U2, 8, VCC)
+ChangePinName(U2, 7, DISC)
+ChangePinName(U2, 6, THRES)
+ChangePinName(U2, 3, OUT)
+ChangePinName(U2, 5, CTRL)
+ChangePinName(U2, 4, \_RESET\_)
+ChangePinName(U2, 2, \_TRIG\_)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 3, ANODE)
+ChangePinName(U1, 2, REF)
+ChangePinName(U1, 1, CATHODE)
+
+# Start of element Q1
+ChangePinName(Q1, 2, E)
+ChangePinName(Q1, 3, C)
+ChangePinName(Q1, 1, B)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element U3
+ChangePinName(U3, 8, P1.6/TA0.1/A6/TDI/TCLK)
+ChangePinName(U3, 9, P1.7/A7/TDO/TDI)
+ChangePinName(U3, 10, RST/NMI/SBWTDIO)
+ChangePinName(U3, 11, TEST/SBWTCK)
+ChangePinName(U3, 12, XOUT/P2.7)
+ChangePinName(U3, 13, XIN/P2.6/TA0.1)
+ChangePinName(U3, 14, DVSS)
+ChangePinName(U3, 7, P1.5/TA0.0/A5/SCLK/TMS)
+ChangePinName(U3, 6, P1.4/SMCLK/A4/REF+/TCK)
+ChangePinName(U3, 5, P1.3/ADC10CLK/A3/REF-)
+ChangePinName(U3, 4, P1.2/TA0.1/A2)
+ChangePinName(U3, 3, P1.1/TA0.0/A1)
+ChangePinName(U3, 2, P1.0/TA0CLK/ACLK/A0)
+ChangePinName(U3, 1, DVCC)
