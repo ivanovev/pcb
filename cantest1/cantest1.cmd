f70cbcdb67d209942812a2dc4dde83a77556c99d
iev <iev@land.ru>
2016-02-15 20:26:12 +0300

    added cantest1.sch

---
+++ cantest1/cantest1.cmd
@@ -0,0 +1,135 @@
+# Pin name action command file
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element TP4
+ChangePinName(TP4, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element U3
+ChangePinName(U3, 3, 3)
+ChangePinName(U3, 2, 2)
+ChangePinName(U3, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element TP3
+ChangePinName(TP3, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element TP2
+ChangePinName(TP2, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element TP1
+ChangePinName(TP1, 1, 1)
+
+# Start of element A1
+ChangePinName(A1, 6, CG)
+ChangePinName(A1, 5, CG)
+ChangePinName(A1, 4, CG)
+ChangePinName(A1, 2, CB)
+ChangePinName(A1, 3, PSG)
+ChangePinName(A1, 1, B)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 5, NR)
+ChangePinName(U2, 3, \_ERR\_)
+ChangePinName(U2, 1, OUT)
+ChangePinName(U2, 4, GND)
+ChangePinName(U2, 6, \_SD\_)
+ChangePinName(U2, 2, IN)
+
+# Start of element J1
+ChangePinName(J1, 11, 11)
+ChangePinName(J1, 12, 12)
+ChangePinName(J1, 13, 13)
+ChangePinName(J1, 14, 14)
+ChangePinName(J1, 15, 15)
+ChangePinName(J1, 16, 16)
+ChangePinName(J1, 19, 19)
+ChangePinName(J1, 17, 17)
+ChangePinName(J1, 20, 20)
+ChangePinName(J1, 18, 18)
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 10, 10)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
+
+# Start of element A2
+ChangePinName(A2, 3, GND)
+ChangePinName(A2, 2, LINE2)
+ChangePinName(A2, 1, LINE1)
+
+# Start of element U1
+ChangePinName(U1, 2, VSS)
+ChangePinName(U1, 5, VIO)
+ChangePinName(U1, 3, VDD)
+ChangePinName(U1, 6, CANL)
+ChangePinName(U1, 7, CANH)
+ChangePinName(U1, 8, STBY)
+ChangePinName(U1, 4, RXD)
+ChangePinName(U1, 1, TXD)
